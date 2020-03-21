784f738f91191ae9b98612bb2e9832ee12bc9a94
Gio <531406@student.glu.nl>
2020-03-21 21:38:12 +0100

    worked on the music bot

--- music.js
+++ music.js
@@ -1,59 +1,97 @@
-const Discord = require("discord.js");
-const fs = require("fs");
-const bot = new Discord.Client({
-  partials: Object.values(["MESSAGE", "CHANNEL"]),
-});
-const botconfig = require("./botconfig.json");
-bot.commands = new Discord.Collection();
-require('dotenv').config()
-const active = new Map()
-bot.on("ready", async () => {
-    console.log(`${bot.user.username} is online on ${bot.guilds.size} servers`);
-});
+const Discord = require('discord.js')
 
-fs.readdir("./commands/", (err, files) => {
-    if (err) console.log(err);
-    let jsfile = files.filter(f => f.split(".").pop() === "js");
-    if (jsfile.length <= 0) {
-      console.log("Couldn't find commands.");
-      return;
+const bot = new Discord.Client()
+
+const config = require('./botconfig.json')
+const ytld = require('ytdl-core')
+
+const prefix = config.prefix
+
+var queue = new Map()
+
+bot.on('ready', () =>{
+  console.log(`I am ready! I am in ${bot.guilds.size} guilds`)
+})
+
+bot.on('message', async message =>{
+  if(message.author.bot) return
+  if(message.content.indexOf(prefix) !== 0) return
+
+  const args = message.content.split(prefix.length).trim().split(/ +/g)
+  const command = args.shift().toLowerCase()
+
+  const serverQueue = queue.get(message.guild.id)
+
+  if(command === 'play'){
+    play(message, serverQueue)
+  }
+})
+
+async function play(message, serverQueue){
+  const args = message.content.split(" ")
+
+  const voiceChannel = message.member.voiceChannel
+  if(!voiceChannel) return message.reply('Je moet in een voice kanaal zitten!')
+  const permission = voiceChannel.permissionsFor(message.client.user)
+  if(!permission.has('CONNECT') || !permission.has('SPEAK')){
+    return message.channel.send('Ik heb geen permissie om het voice kanaal te joinen')
+  }
+
+  const songInfo = await ytld.getInfo(args[1])
+  const song ={
+    title:songInfo.title, 
+    url:songInfo.video_url
+  }
+
+  if(!serverQueue){
+    const queueConstruct ={
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume:5,
+      playing:true
     }
+
+    queue.set(message.guild.id, queueConstruct)
+
+    queueConstruct.songs.push(song)
+
+    try{
+      var connection = await voiceChannel.join()
+      queueConstruct.connection = connection
+      playSong(message.guild, queueConstruct.songs[0])
+    }catch(err){
+      console.log(err)
+      queue.delete(message.guild.id)
+      return message.channel.send('Er was een error tijdens het afspelen! ' + err)
+    }
+  }else{
+    serverQueue.songs.push(song)
+    return message.channel.send(`${song.title} is toegevoegd aan de wachtrij`)
+  }
+}
+
+
+function playSong(guild, song){
+  const serverQueue = queue.get(guild.id)
   
-    jsfile.forEach((f, i) => {
-      let props = require(`./commands/${f}`);
-      console.log(`${f} loaded!`);
-      bot.commands.set(props.help.name, props);
-    });
-});
-
-
-bot.on("error", console.error);
-
-bot.on("message", async message => {
-  if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
-  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
-  if (!prefixes[message.guild.id]) {
-    prefixes[message.guild.id] = {
-      prefixes: botconfig.prefix
-    };
+  if(!song){
+    serverQueue.voiceChannel.leave()
+    queue.delete(guild.id)
+    return
   }
 
+  const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+    .on('end', ()=>{
+      serverQueue.songs.shift()
+      playSong(guild, serverQueue.songs[0])
+    })
+    .on('error', error =>{
+      console.log(error)
+    })
 
-
-  let prefix = prefixes[message.guild.id].prefixes;
-  if (!message.content.startsWith(prefix)) return;
-
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  var options ={
-      active:active
-  }
-
-
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if (commandfile) commandfile.run(bot, message, args, options);
-});
+  dispatcher.setVolumeLogarithmic(serverQueue.volume /5)
+}
 
 bot.login(process.env.TOKEN)
