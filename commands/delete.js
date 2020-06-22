7d93c32d57997a5e870ee6e5267ee36446f95b40
Gio <531406@student.glu.nl>
2020-06-22 17:13:57 +0200

    uwu

--- commands/delete.js
+++ commands/delete.js
@@ -2,13 +2,33 @@ const Discord = require("discord.js");
 const Errors = require("../utils/errors");
 
 module.exports.run = async (bot, message, args, con) => {
-  message.channel.fetchMessages().then((messages) => {
-    message.channel.bulkDelete(messages);
-    messagesDeleted = messages.array().length;
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+    Errors.noPerms(message, "MANAGE_MESSAGES");
+  }
 
-    message.channel.send(`Deleted ${messagesDeleted} messages`)
+  if (message.author.bot) return;
 
-  });
+  let messagesDeleted = await clearChannel(message.channel);
+
+  message.channel.send("Number of deleted messages:" + messagesDeleted.length);
+
+  async function clearChannel(channel, n = 0, old = false) {
+    let collected = await channel.fetchMessages();
+    if (collected.size > 0) {
+      if (old) {
+        for (let msg of collected.array()) {
+          await msg.delete();
+          n++;
+        }
+      } else {
+        let deleted = await channel.bulkDelete(100, true);
+        if (deleted.size < collected.size) old = true;
+        n += deleted;
+      }
+
+      return n + (await clearChannel(channel, old));
+    } else return 0;
+  }
 };
 
 module.exports.help = {
